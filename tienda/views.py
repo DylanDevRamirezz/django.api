069bad5c75594b3edd4806d9b29ce972d75cf147
DylanDevRamirezz <159856318+DylanDevRamirezz@users.noreply.github.com>
2025-09-13 18:38:23 +0000

    Organizacion de carpetas y creacion de lista de clientes

--- tienda/views.py
+++ tienda/views.py
@@ -8,22 +8,22 @@ def home(request):
     # render () Recibe: request, ruta  template, contexto(Diccionario)
     return render(request, "tienda/home.html", {})
 
-
 def lista_productos(request):
     productos = Producto.objects.all().order_by("nombre")
-    print(productos)
-    return render(request, "tienda/lista_productos.html", {"productos": productos})
+    return render(request, "tienda/producto/lista_productos.html", {"productos": productos})
 
-
-def detalle_producto(request, pk):
-    producto = get_object_or_404(Producto, pk=pk)
-    return render(request, "tienda/detalle_producto.html", {"producto": producto})
-
+def lista_clientes(request):
+    clientes = Cliente.objects.all().order_by("nombre")
+    return render(request, "tienda/cliente/lista_clientes.html", {"clientes": clientes})
 
 def lista_pedidos(request):
     pedido = Pedido.objects.select_related("cliente").prefetch_related(
         "productos").order_by("-fecha")
-    return render(request, "tienda/lista_pedidos.html", {"pedido": pedido})
+    return render(request, "tienda/pedido/lista_pedidos.html", {"pedido": pedido})
+
+def detalle_producto(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    return render(request, "tienda/producto/detalle_producto.html", {"producto": producto})
 
 def detalle_pedido(request, pk):
     pedido = get_object_or_404(Pedido.objects.select_related("cliente")).prefetch_related("productos", pk=pk)
@@ -32,7 +32,8 @@ def detalle_pedido(request, pk):
 def detalle_cliente(request, pk):
     cliente = get_object_or_404(Cliente, pk=pk)
     pedidos = Cliente.pedidos.select_related("cliente".prefetch_related("productos")).order_by("-fecha")
-    return render(request, "tienda/detalle_cliente.html", {"cliente": cliente, "pedidos": pedidos })
+    return render(request, "tienda/cliente/detalle_cliente.html", {"cliente": cliente, "pedidos": pedidos })
+
 
 def crear_producto(request):
     if request.method == 'POST':
@@ -42,7 +43,7 @@ def crear_producto(request):
             return redirect('tienda:lista_productos')
     else:
         form = ProductoForm()
-    return render(request, 'tienda/crear_producto.html', {'form': form})
+    return render(request, 'tienda/producto/crear_producto.html', {'form': form})
 
 def modificar_producto(request, pk):
     producto = get_object_or_404(Producto, pk=pk)
@@ -54,4 +55,4 @@ def modificar_producto(request, pk):
     else:
         form = ProductoForm(instance=producto)
     
-    return render(request, 'tienda/modificar_producto.html', {'form': form, 'producto': producto})
+    return render(request, 'tienda/producto/modificar_producto.html', {'form': form, 'producto': producto})
